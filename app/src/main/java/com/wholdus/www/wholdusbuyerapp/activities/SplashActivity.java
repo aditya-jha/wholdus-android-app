f464f68cb6b210c70c2825e10f842d4b319c61d9
Aditya Jha <batu.aditya007@gmail.com>
2016-11-14 00:27:17 +0530

    experimenting with splash and animation

--- app/src/main/java/com/wholdus/www/wholdusbuyerapp/activities/SplashActivity.java
+++ app/src/main/java/com/wholdus/www/wholdusbuyerapp/activities/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.wholdus.www.wholdusbuyerapp.activities;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,29 +12,21 @@ import com.wholdus.www.wholdusbuyerapp.aynctasks.LoginHelperAsyncTask;
 public class SplashActivity extends AppCompatActivity {
 
     private final String LOG_TAG = "SplashActivity";
-    private final long WAITING_TIME = 600;
-    private long mStartTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activty_splash);
-        Log.v(LOG_TAG, "on create");
-
-        mStartTime = System.currentTimeMillis();
         proceed();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.v(LOG_TAG, "on resume");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.v(LOG_TAG, "on destroy");
     }
 
     private void proceed() {
@@ -43,30 +36,11 @@ public class SplashActivity extends AppCompatActivity {
 
                 final Boolean output = result;
 
-                Runnable runnable = new Runnable() {
-                    @Override
-                    public void run() {
-                        long currentTime = System.currentTimeMillis();
-                        long timeDiff = currentTime - mStartTime;
-
-                        if(timeDiff < WAITING_TIME) {
-                            try {
-                                Log.v(LOG_TAG, "waiting for: " + (WAITING_TIME - timeDiff));
-                                Thread.sleep(WAITING_TIME - timeDiff);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                        }
-
-                        if(output) {
-                            Log.v(LOG_TAG, "user already logged in");
-                        } else {
-                            startLoginSignupActivity();
-                        }
-                    }
-                };
-
-                new Thread(runnable).start();
+                if(output) {
+                    Log.v(LOG_TAG, "user already logged in");
+                } else {
+                    startLoginSignupActivity();
+                }
             }
         });
 
@@ -78,5 +52,6 @@ public class SplashActivity extends AppCompatActivity {
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
         finish();
+        overridePendingTransition(0, 0);
     }
 }
