53543a1404277bdbae9082c6524e133293da2aa7
Aditya Jha <batu.aditya007@gmail.com>
2016-11-19 13:02:12 +0530

    created orders fragment and related layout file

---
+++ app/src/main/java/com/wholdus/www/wholdusbuyerapp/fragments/OrdersFragment.java
@@ -0,0 +1,26 @@
+package com.wholdus.www.wholdusbuyerapp.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.wholdus.www.wholdusbuyerapp.R;
+
+/**
+ * Created by aditya on 19/11/16.
+ */
+
+public class OrdersFragment extends Fragment {
+
+    public OrdersFragment() {}
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_orders, container, false);
+        return rootView;
+    }
+}
