88aef76519a62e27290674177e3894fc81dffc5a
Aditya Jha <batu.aditya007@gmail.com>
2016-11-19 12:57:07 +0530

    created empty account activity

---
+++ app/src/main/java/com/wholdus/www/wholdusbuyerapp/activities/AccountActivity.java
@@ -0,0 +1,15 @@
+package com.wholdus.www.wholdusbuyerapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wholdus.www.wholdusbuyerapp.R;
+
+public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+}
