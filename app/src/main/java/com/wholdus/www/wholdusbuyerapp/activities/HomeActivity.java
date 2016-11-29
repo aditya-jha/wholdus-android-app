5a8f5842931365acb0f9a8275ded974f3e3468da
Aditya Jha <batu.aditya007@gmail.com>
2016-11-29 12:30:22 +0530

    modified gitignore

--- app/src/main/java/com/wholdus/www/wholdusbuyerapp/activities/HomeActivity.java
+++ app/src/main/java/com/wholdus/www/wholdusbuyerapp/activities/HomeActivity.java
@@ -42,7 +42,7 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         if(!mDoublePressToExit) {
-            
+
         }
         super.onBackPressed();
     }
