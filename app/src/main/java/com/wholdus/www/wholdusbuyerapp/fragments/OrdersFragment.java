578c84cd53fa1851bb8c6a340728c03632d65f7c
Kaustubh <kausthegreat@gmail.com>
2016-12-06 16:53:45 +0530

    working on orders fragment

--- app/src/main/java/com/wholdus/www/wholdusbuyerapp/fragments/OrdersFragment.java
+++ app/src/main/java/com/wholdus/www/wholdusbuyerapp/fragments/OrdersFragment.java
@@ -1,13 +1,19 @@
 package com.wholdus.www.wholdusbuyerapp.fragments;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.LocalBroadcastManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.wholdus.www.wholdusbuyerapp.R;
+import com.wholdus.www.wholdusbuyerapp.interfaces.ProfileListenerInterface;
+import com.wholdus.www.wholdusbuyerapp.services.UserService;
 
 /**
  * Created by aditya on 19/11/16.
@@ -15,12 +21,31 @@ import com.wholdus.www.wholdusbuyerapp.R;
 
 public class OrdersFragment extends Fragment {
 
+    private ProfileListenerInterface mListener;
+
     public OrdersFragment() {}
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            mListener = (ProfileListenerInterface) context;
+        } catch (ClassCastException cee) {
+            cee.printStackTrace();
+        }
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_orders, container, false);
         return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mListener.fragmentCreated("My Orders", true);
+
     }
 }
