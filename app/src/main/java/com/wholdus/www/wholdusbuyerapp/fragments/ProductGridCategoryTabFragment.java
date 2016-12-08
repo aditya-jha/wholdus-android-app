2fd9ccc28daa4862409d6fbf181c10becea42f88
Aditya Jha <batu.aditya007@gmail.com>
2016-12-08 17:09:32 +0530

    removed old fragment and created new for rejected products

---
+++ app/src/main/java/com/wholdus/www/wholdusbuyerapp/fragments/ProductGridCategoryTabFragment.java
@@ -0,0 +1,65 @@
+package com.wholdus.www.wholdusbuyerapp.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.wholdus.www.wholdusbuyerapp.R;
+
+/**
+ * Created by aditya on 8/12/16.
+ * Fragment to show product grid and category tabs
+ */
+
+public class ProductGridCategoryTabFragment extends Fragment {
+
+    public ProductGridCategoryTabFragment() {
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_product_grid_category_tab, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
