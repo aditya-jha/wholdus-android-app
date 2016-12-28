155b3c43a435f0c10ec1a9a03a6fc0e5e55126a2
Aditya Jha <batu.aditya007@gmail.com>
2016-12-28 12:19:57 +0530

    added boilerplate for fcm

---
+++ app/src/main/java/com/wholdus/www/wholdusbuyerapp/services/WholdusFirebaseInstanceIDService.java
@@ -0,0 +1,15 @@
+package com.wholdus.www.wholdusbuyerapp.services;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by aditya on 28/12/16.
+ */
+
+public class WholdusFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+    }
+}
