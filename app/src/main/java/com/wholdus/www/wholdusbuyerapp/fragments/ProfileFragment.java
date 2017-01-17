026096e39437a738202ce676c0f8ffe18a978c54
Aditya Jha <adityaiitr@hotmail.com>
2017-01-17 13:05:00 +0530

    profile fragment finished

--- app/src/main/java/com/wholdus/www/wholdusbuyerapp/fragments/ProfileFragment.java
+++ app/src/main/java/com/wholdus/www/wholdusbuyerapp/fragments/ProfileFragment.java
@@ -10,28 +10,25 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.wholdus.www.wholdusbuyerapp.R;
-import com.wholdus.www.wholdusbuyerapp.adapters.AddressDisplayListViewAdapter;
 import com.wholdus.www.wholdusbuyerapp.adapters.BuyerPersonalDetailsAdapter;
 import com.wholdus.www.wholdusbuyerapp.helperClasses.HelperFunctions;
 import com.wholdus.www.wholdusbuyerapp.helperClasses.TODO;
 import com.wholdus.www.wholdusbuyerapp.interfaces.ProfileListenerInterface;
 import com.wholdus.www.wholdusbuyerapp.loaders.ProfileLoader;
 import com.wholdus.www.wholdusbuyerapp.models.Buyer;
-import com.wholdus.www.wholdusbuyerapp.models.BuyerAddress;
 import com.wholdus.www.wholdusbuyerapp.models.BuyerPersonalDetails;
 import com.wholdus.www.wholdusbuyerapp.services.UserService;
 
 import java.util.ArrayList;
-
-import static com.wholdus.www.wholdusbuyerapp.helperClasses.TODO.FETCH_USER_PROFILE;
 
 /**
  * Created by aditya on 19/11/16.
@@ -44,7 +41,10 @@ public class ProfileFragment extends Fragment implements LoaderManager.LoaderCal
     private ListView mPersonalDetailsListView;
     private BroadcastReceiver mUserServiceResponseReceiver;
     private ProfileListenerInterface mListener;
-    private final int USER_DB_LOADER = 0;
+    private ProgressBar mPageLoader;
+    private CardView mProfileCard;
+
+    private static final int USER_DB_LOADER = 0;
 
     public ProfileFragment() {
     }
@@ -52,11 +52,7 @@ public class ProfileFragment extends Fragment implements LoaderManager.LoaderCal
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        try {
-            mListener = (ProfileListenerInterface) context;
-        } catch (ClassCastException cee) {
-            cee.printStackTrace();
-        }
+        mListener = (ProfileListenerInterface) context;
     }
 
     @Override
@@ -73,14 +69,31 @@ public class ProfileFragment extends Fragment implements LoaderManager.LoaderCal
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_profile, container, false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
 
-        initReferences(rootView);
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mPageLoader = (ProgressBar) view.findViewById(R.id.page_loader);
+
+        mProfileCard = (CardView) view.findViewById(R.id.profile_card);
+        mProfileCard.setVisibility(View.INVISIBLE);
+
+        mPersonalDetailsListView = (ListView) view.findViewById(R.id.personal_details_list_view);
+
+        TextView mEditPersonalDetailsTextView = (TextView) view.findViewById(R.id.edit_personal_details_text_view);
+        mEditPersonalDetailsTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mListener.editPersonalDetails();
+            }
+        });
+
         getActivity().getSupportLoaderManager().restartLoader(USER_DB_LOADER, null, this);
 
         fetchDataFromServer();
-
-        return rootView;
     }
 
     @Override
@@ -108,7 +121,6 @@ public class ProfileFragment extends Fragment implements LoaderManager.LoaderCal
     public void onDestroy() {
         super.onDestroy();
         mUserServiceResponseReceiver = null;
-        //TODO : Do nullifying receivers for all fragments and activities
     }
 
     @Override
@@ -118,19 +130,32 @@ public class ProfileFragment extends Fragment implements LoaderManager.LoaderCal
     }
 
     @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
     public Loader<Buyer> onCreateLoader(int id, Bundle args) {
         return new ProfileLoader(getContext(), true, false, false);
     }
 
     @Override
-    public void onLoadFinished(Loader<Buyer> loader, Buyer data) {
-        setViewForPersonalDetails(data);
+    public void onLoadFinished(Loader<Buyer> loader, Buyer buyer) {
+        if (buyer == null) return;
 
+        mPageLoader.setVisibility(View.INVISIBLE);
+        mProfileCard.setVisibility(View.VISIBLE);
+
+        ArrayList<BuyerPersonalDetails> items = new ArrayList<>();
+        items.add(new BuyerPersonalDetails(getString(R.string.name_key),
+                buyer.getName().isEmpty() ? getString(R.string.value_not_provided) : buyer.getName(), R.drawable.ic_person_black_24dp));
+        items.add(new BuyerPersonalDetails(getString(R.string.company_name_key),
+                buyer.getCompanyName().isEmpty() ? getString(R.string.value_not_provided) : buyer.getCompanyName(), R.drawable.ic_store_mall_directory_black_28dp));
+        items.add(new BuyerPersonalDetails(getString(R.string.mobile_number_key),
+                buyer.getMobileNumber(), R.drawable.ic_phone_black_24dp));
+        items.add(new BuyerPersonalDetails(getString(R.string.email_key),
+                buyer.getEmail().isEmpty() ? getString(R.string.value_not_provided) : buyer.getEmail(), R.drawable.ic_mail_outline_black_24dp));
+        items.add(new BuyerPersonalDetails(getString(R.string.whatsapp_number_key),
+                buyer.getWhatsappNumber().isEmpty() ? getString(R.string.value_not_provided) : buyer.getWhatsappNumber(), R.drawable.ic_perm_phone_msg_black_24dp));
+
+        BuyerPersonalDetailsAdapter adapter = new BuyerPersonalDetailsAdapter(getContext(), items);
+        mPersonalDetailsListView.setAdapter(adapter);
+        HelperFunctions.setListViewHeightBasedOnChildren(mPersonalDetailsListView);
     }
 
     @Override
@@ -138,43 +163,9 @@ public class ProfileFragment extends Fragment implements LoaderManager.LoaderCal
 
     }
 
-    private void initReferences(ViewGroup rootView) {
-        mPersonalDetailsListView = (ListView) rootView.findViewById(R.id.personal_details_list_view);
-
-        TextView mEditPersonalDetailsTextView = (TextView) rootView.findViewById(R.id.edit_personal_details_text_view);
-        mEditPersonalDetailsTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mListener.editPersonalDetails();
-            }
-        });
-
-    }
-
     private void handleAPIResponse() {
         getActivity().getSupportLoaderManager().restartLoader(USER_DB_LOADER, null, this);
     }
-
-    private void setViewForPersonalDetails(Buyer buyer) {
-        if (buyer == null) return;
-
-        ArrayList<BuyerPersonalDetails> items = new ArrayList<>();
-        items.add(new BuyerPersonalDetails(getString(R.string.name_key),
-                buyer.getName(), R.drawable.ic_person_black_24dp));
-        items.add(new BuyerPersonalDetails(getString(R.string.company_name_key),
-                buyer.getCompanyName(), R.drawable.ic_store_mall_directory_black_28dp));
-        items.add(new BuyerPersonalDetails(getString(R.string.mobile_number_key),
-                buyer.getMobileNumber(), R.drawable.ic_phone_black_24dp));
-        items.add(new BuyerPersonalDetails(getString(R.string.email_key),
-                buyer.getEmail(), R.drawable.ic_mail_outline_black_24dp));
-        items.add(new BuyerPersonalDetails(getString(R.string.whatsapp_number_key),
-                buyer.getWhatsappNumber(), R.drawable.ic_perm_phone_msg_black_24dp));
-
-        BuyerPersonalDetailsAdapter adapter = new BuyerPersonalDetailsAdapter(getContext(), items);
-        mPersonalDetailsListView.setAdapter(adapter);
-        HelperFunctions.setListViewHeightBasedOnChildren(mPersonalDetailsListView);
-    }
-
 
     private void fetchDataFromServer() {
         Intent intent = new Intent(getContext(), UserService.class);
